6f3b647ae8f11be2c8be119334d10bb3cd53f21a
Bighneshi <74350221+Bighneshi@users.noreply.github.com>
2021-10-01 14:54:45 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,8 +8,12 @@ function preload(){
 
 
 function setup() { 
-  canvas = createCanvas(640, 420); 
+  canvas = createCanvas(380, 380); 
   canvas.center(); 
+  video = createCapture(VIDEO);
+  video.size(380, 380);
+  video.hide();
+  video.size(380, 380);
   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
   document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
@@ -18,7 +22,7 @@ function setup() {
 function modelLoaded() {
 console.log("Model Loaded!");
 status = true; 
-objectDetector.detect(img, gotResult);
+objectDetector.detect(video, gotResult);
 } 
 
 function gotResult(error, results) {
@@ -30,18 +34,23 @@ function gotResult(error, results) {
 }
 
 function draw() { 
-  image(img, 0, 0, 640, 420); 
+  image(video, 0, 0, 380, 380); 
   
        if(status !="") 
        {
+         r = random(255);
+         g = random(255);
+         b = random(255);
+         objectDetector.detect(video, gotResult);
          for (i = 0; i <objects.length; i++) {
            document.getElementById("status").innerHTML = "Status: Object Detected";
-
-           fill("#FF0000");
+           document.getElementById("number_of_objects").innerHTML = " Number of objects are : "+ objects.length;
+           
+           fill(r,g,b);
            percent = floor(objects[i].confidence * 100);
            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
            noFill();
-           stroke("#FF0000");
+           stroke(r,g,b);
            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);  
          }
        }
